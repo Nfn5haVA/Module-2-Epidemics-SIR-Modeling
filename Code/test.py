c2a0e7d957a4553d8daa07413eb64e5485debc6b
Roy Chen <nfn5ha@virginia.edu>
2026-03-17 15:37:16 -0400

    update

--- Code/test.py
+++ Code/test.py
@@ -4,4 +4,16 @@ matplotlib.use("Agg")
 import pandas as pd
 import matplotlib.pyplot as plt
 
-print("ST
+print("START")
+
+data = pd.read_csv(
+    "Data/mystery_virus_daily_active_counts_RELEASE#1.csv"
+)
+
+plt.scatter(data['day'], data['active reported daily cases'])
+plt.title("Active Cases Over Time")
+plt.xlabel("Time (days)")
+plt.ylabel("Active Reported Daily Cases")
+plt.savefig("active_cases_plot.png")
+
+print("END")
